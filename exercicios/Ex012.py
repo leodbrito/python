de9176cfe0adb23eed46f40a89760e705d7fd008
Leonardo Brito <36524761+leodbrito@users.noreply.github.com>
2018-11-21 19:23:51 -0200

    Add files via upload

--- exercicios/Ex012.py
+++ exercicios/Ex012.py
@@ -3,4 +3,4 @@
 # novo preço, com 5% de desconto.
 
 p = float(input('Digite o preço de um produto: '))
-print('Este produto custará R${:.2f} com 5% de desconto!'.format(p-p*0.05))
+print('\nEste produto custará R${:.2f} com 5% de desconto!'.format(p-p*0.05))
---
+++ exercicios/Ex013.py
@@ -0,0 +1,6 @@
+# Ex013: faça um algoritmo que leia
+# o salário de um funcionário e mostre
+# seu novo salário com 15% de aumento
+
+s = float(input('Digite seu salário: '))
+print('\nSeu salário será R${:.2f} com 15% de aumento!'.format(s*1.15))
