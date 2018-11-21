7f313d91bce3494bbdee901336bde1b346bfb0f6
Leonardo Brito <36524761+leodbrito@users.noreply.github.com>
2018-11-21 16:48:33 -0200

    Add files via upload

---
+++ exercicios/Ex006.py
@@ -0,0 +1,8 @@
+# Ex006: Crie um algoritmo que leia um
+# número e mostre seu dobro, triplo e
+# raiz quadrada
+
+n = int(input('Digite um numero inteiro: '))
+print('Seu dobro é {}'.format(n*2))
+print('Seu triplo é {}'.format(n*3))
+print('Sua raiz quadrada é {}'.format(n**(1/2)))
