fc9acb972422b41892214e8cc8fcf0ba4c7b1193
Leonardo Brito <36524761+leodbrito@users.noreply.github.com>
2018-11-21 17:07:38 -0200

    Add files via upload

---
+++ exercicios/Ex008.py
@@ -0,0 +1,6 @@
+# Ex008: Escreva um programa que
+# leia um valor em metros e o exiba
+# convertido em centimetros e milimetros.
+
+v = int(input('Digite um valor em metros: '))
+print('Este valor equivale a {} centimetors!'.format(v*100))
